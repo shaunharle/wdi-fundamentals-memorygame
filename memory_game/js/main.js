bf9025a05331ed98bcb4b474004048321189f07d
Shaun Harle <shaunharle@Shauns-MacBook-Pro.local>
2019-09-20 00:12:33 -0400

    finished building the general game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -19,7 +19,7 @@ cardImage: "images/king-of-hearts.png",
 {
 rank: "king",
 suit: "diamonds",
-cardImage: "images/king-of-diamonds",
+cardImage: "images/king-of-diamonds.png",
 },
 ];
 
@@ -27,25 +27,36 @@ var cardsInPlay = [];
 
 
 function checkForMatch(){
-	if (cardsInPlay[0] === cardsInPlay[1]) {
+	if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
 		alert("You found a match!")
 	} else {
 		alert("Sorry, try again.")
 	}
 }
 
-function flipCard(cardId) {
+function createBoard() {
+	for (var i=0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log("User flipped " + cards[cardId].suit);
 	console.log("User flipped " + cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId]).rank;
+	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2){
 		checkForMatch();
 	}
 }
 
-flipCard(0);
-flipCard(2);
+createBoard();
 
 
 
