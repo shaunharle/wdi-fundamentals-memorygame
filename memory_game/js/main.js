04efbbc1e984b2248d5233150cd929ea9254c011
Shaun Harle <shaunharle@Shauns-MacBook-Pro.local>
2019-09-19 23:17:27 -0400

    added ranks and suits

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,28 @@
 
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png",
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png",
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png",
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds",
+},
+];
+
 var cardsInPlay = [];
 
 
@@ -13,8 +35,10 @@ function checkForMatch(){
 }
 
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].suit);
+	console.log("User flipped " + cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId]).rank;
 	if (cardsInPlay.length === 2){
 		checkForMatch();
 	}
